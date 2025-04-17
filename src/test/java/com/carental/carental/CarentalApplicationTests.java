6068b8b3dfd99ec2a1203bae12fc7ca4c526f3b1
antocreadev <anthonymenghi@icloud.com>
2025-04-17 09:45:04 +0200

    init

---
+++ src/test/java/com/carental/carental/CarentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carental.carental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
