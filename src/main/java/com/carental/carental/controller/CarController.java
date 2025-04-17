ab7a0d86dd55bfb9221718aacb1d4573425bb790
antocreadev <anthonymenghi@icloud.com>
2025-04-17 09:54:44 +0200

    add: CarController pour gérer les opérations liées aux voitures & update: dependace dans pom.xml pour springframework.web

---
+++ src/main/java/com/carental/carental/controller/CarController.java
@@ -0,0 +1,28 @@
+package com.carental.carental.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+
+    @Autowired
+    private CarRentalService carRentalService;
+
+    @GetMapping
+    public List<Car> getAllCars() {
+        return carRentalService.getAllCars();
+    }
+
+    @PostMapping("/rent/{registrationNumber}")
+    public boolean rentCar(@PathVariable String registrationNumber) {
+        return carRentalService.rentCar(registrationNumber);
+    }
+
+    @PostMapping("/return/{registrationNumber}")
+    public void returnCar(@PathVariable String registrationNumber) {
+        carRentalService.returnCar(registrationNumber);
+    }
+}
