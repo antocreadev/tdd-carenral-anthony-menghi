28fe5d354f9258fd78e71d970a33de19c28c7d97
antocreadev <anthonymenghi@icloud.com>
2025-04-17 10:34:45 +0200

    ajout de la méthode addCar dans CarController pour permettre l'ajout de nouvelles voitures

--- src/main/java/com/carental/carental/controller/CarController.java
+++ src/main/java/com/carental/carental/controller/CarController.java
@@ -1,11 +1,16 @@
 package com.carental.carental.controller;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.carental.carental.domain.Car;
 import com.carental.carental.service.CarRentalService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
@@ -28,4 +33,9 @@ public class CarController {
     public void returnCar(@PathVariable String registrationNumber) {
         carRentalService.returnCar(registrationNumber);
     }
+    @PostMapping("/add")
+    public boolean addCar(@RequestBody Car car) {
+    return carRentalService.addCar(car);
+    }
+
 }
